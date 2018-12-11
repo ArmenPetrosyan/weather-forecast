c06fa90b6977f17272e24c0e7a08304d2a70685c
ArmenPetrosyan <thebraincrasher@gmail.com>
2018-12-11 02:44:53 +0200

    Add webpack && App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, {Component, Fragment} from 'react';
+
+class App extends React.Component {
+    render () {
+        return (
+            <Fragment>
+                <header>
+                    <h1>Hello app</h1>
+                </header>
+                <main>
+                    <ul>
+                        <li>Asaldkaldkla</li>
+                        <li>dsdasdsad</li>
+                        <li>fasfsafasfasfas</li>
+                        <li>wretwetw</li>
+                        <li>reee</li>
+                    </ul>
+                </main>
+                <footer>
+                    <span>&copy; Hello app</span>
+                </footer>
+            </Fragment>    
+        )
+    }
+}
+
+export default App;
