e1d7af2f22e20447a09208e795146343f909b4e3
ArmenPetrosyan <thebraincrasher@gmail.com>
2019-01-14 13:18:59 +0200

    Icon component updated - added props validation

--- src/view/components/Icon/Icon.jsx
+++ src/view/components/Icon/Icon.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Icon = ({ icon, width = 32, height = 32 }) => (
-  <svg width={width} height={height} className={`icon icon-${icon}`}>
-    <use xlinkHref={`#${icon}`} />
-  </svg>
-);
+const Icon = (props) => {
+  const {
+    icon, width, height, className, ...rest
+  } = props;
+
+  return (
+    <svg
+      width={width}
+      height={height}
+      className={`${className} Icon Icon-${icon}`}
+      {...rest}
+    >
+      <use xlinkHref={`#${icon}`} />
+    </svg>
+  );
+};
+
+Icon.propTypes = {
+  icon: PropTypes.string.isRequired,
+  width: PropTypes.number,
+  height: PropTypes.number,
+  className: PropTypes.string,
+};
+
+Icon.defaultProps = {
+  width: 32,
+  height: 32,
+  className: '',
+};
 
 export default Icon;
