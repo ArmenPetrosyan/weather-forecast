dfdd6f14d2d28a84683210ea1483b5016cd1d461
ArmenPetrosyan <thebraincrasher@gmail.com>
2019-01-16 00:02:09 +0200

    Changed styles

--- src/view/containers/Header/Header.jsx
+++ src/view/containers/Header/Header.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { withNamespaces } from 'react-i18next';
 import styles from './Header.scss';
-import typography from 'View/styles/Typography.scss';
 import base from 'Root/App.scss';
 
 import { DateWidget } from 'Components';
@@ -18,7 +17,7 @@ class Header extends React.Component {
     return (
       <header className={styles.Header}>
         <div className={base.Container}>
-          <h1 className={typography.H0}>
+          <h1 className={styles['Header--title']}>
             { t('The weather monitor') }
           </h1>
           <DateWidget dateObject={new Date()} />
