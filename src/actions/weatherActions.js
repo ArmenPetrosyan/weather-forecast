d9088bc153bb1d26e65ab6ac4cb074c8a79f7ec6
ArmenPetrosyan <thebraincrasher@gmail.com>
2019-01-15 02:14:26 +0200

    Cleaned action

--- src/actions/weatherActions.js
+++ src/actions/weatherActions.js
@@ -1,10 +1,8 @@
 import getWeatherByCity from '../api/weather';
 
-const getInstantWeather = (cityName) => {
-  console.log("getInstantWeather");
+const getInstantWeather = (cityName = 'kyiv') => {
   return async (dispatch) => {
-    const result = await getWeatherByCity('kyiv');
-    console.log(result);
+    const result = await getWeatherByCity(cityName);
     dispatch({ type: 'GET_INSTANT_WEATHER_SUCCESS', payload: { weather: result } });
   };
 };
